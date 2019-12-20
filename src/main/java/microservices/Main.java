eda14a411185055edb7a38c9ecd0038b6393b14e
Luis Mejia <luyisimiger@gmail.com>
2019-12-20 08:23:58 -0500

    config inicial del proyecto

---
+++ src/main/java/microservices/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package microservices;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author luyisimiger
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+}
+
